fad55e480196ba8baf79a820900eb3d1f2f8d893
Jannes Münchmeyer <munchmej@univ-grenoble-alpes.fr>
2023-10-16 18:13:14 +0200

    Bump version to v0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ from datetime import datetime
 project = "PyOcto"
 copyright = f"{datetime.now().year}, Jannes Münchmeyer"
 author = "Jannes Münchmeyer"
-release = "0.0.1"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
